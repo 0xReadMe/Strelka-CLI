605489b2f0f6540bfb26d9ab36e23c81e58e90a2
Alt0123 <muhin.2002@yandex.ru>
2023-01-12 08:58:06 +0300

    добавил эксепшены аккаунта и парсера

---
+++ Strelka/Exceptions/AccountEx.cs
@@ -0,0 +1,14 @@
+﻿namespace Strelka_DLL;
+
+[Serializable]
+public class AccountEx : Exception
+{
+    public AccountEx()
+    { }
+    public AccountEx(string message)
+        : base(message)
+    { }
+    public AccountEx(string message, Exception innerException)
+        : base(message, innerException)
+    { }
+}
---
+++ Strelka/Exceptions/ParserEx.cs
@@ -0,0 +1,14 @@
+﻿namespace Strelka_DLL;
+
+[Serializable]
+public class ParserEx : Exception
+{
+    public ParserEx()
+    { }
+    public ParserEx(string message)
+        : base(message)
+    { }
+    public ParserEx(string message, Exception innerException)
+        : base(message, innerException)
+    { }
+}
